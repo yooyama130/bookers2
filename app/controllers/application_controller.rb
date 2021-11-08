2d3a3e94df5b18274aa002bf401eb5597245baa6
EC2 Default User <yooyama.97.01@gmail.com>
2021-11-08 08:47:51 +0000

    [Update] topページの作成 + サインアップ、サインイン後のリダイレクト先を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # ログイン後のリダイレクト処理
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
 
   protected
 
