eccecd684d11a22b83238a06efbbc6b3b8521a3d
EC2 Default User <yooyama.97.01@gmail.com>
2021-11-08 05:26:27 +0000

    [Update] 共通のヘッダーとフッターを作成 + 新規登録、ログイン画面をユーザー名を扱えるように

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :book
+  root to: 'homes#top'
+  get '/homes/about' => 'homes#about'
+  resources :books
+  resources :users, only: [:show]
 end
