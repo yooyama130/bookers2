a8098f82ba2c7fb7719f88ac5302e9aff779bf0f
EC2 Default User <yooyama.97.01@gmail.com>
2021-11-09 09:12:13 +0000

    [Update] userのindexページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
+  def index
+    @user = current_user
+    @new_book = Book.new
+    @all_users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
-    @all_users = User.all;
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get '/homes/about' => 'homes#about'
   resources :books
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show,:index, :edit]
 end
