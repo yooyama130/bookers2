f237f0110356cca851c363b96fdb8c679fdcfedf
EC2 Default User <yooyama.97.01@gmail.com>
2021-11-09 07:17:30 +0000

    [Fix] bookのindexページのshowリンク修正し、 すべての本が一覧に並ぶように + [Update] bookのcreate機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,24 @@
 class BooksController < ApplicationController
   def index
     @user = current_user
+    @all_users = User.all
+    @all_books = Book.all
     @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
