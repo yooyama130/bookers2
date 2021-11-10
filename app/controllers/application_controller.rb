a7ea85ae080f4a57f4fca4fac3eca9de4c3c05c6
EC2 Default User <yooyama.97.01@gmail.com>
2021-11-10 04:15:15 +0000

    [Update] ログイン、ログアウト成功時のフラッシュメッセージを実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,14 @@ class ApplicationController < ActionController::Base
 
   # ログイン後のリダイレクト処理
   def after_sign_in_path_for(resource)
+    flash[:success] = 'Signed in successfully.'
     user_path(resource)
+  end
+
+  # ログアウト後のリダイレクト処理
+  def after_sign_out_path_for(resource)
+    flash[:success] = 'Signed out successfully.'
+    root_path
   end
 
   protected
