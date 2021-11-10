370b5e3f0b53a04d7d4ca33aad3cf3405583c145
EC2 Default User <yooyama.97.01@gmail.com>
2021-11-10 00:56:11 +0000

    [Update] userのshowページ、bookのeditページ、update、destroy機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,6 +18,22 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @new_book = Book.new
   end
 end
