0473c13f68fef0569af37a7bb907f3214e9d9a72
EC2 Default User <yooyama.97.01@gmail.com>
2021-11-10 08:51:52 +0000

    [Fix] userカラムbodyをintroductionに改名

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image, :body)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
