846278d9cf39be15b4145e9bc9707dd76a7a37c0
EC2 Default User <yooyama.97.01@gmail.com>
2021-11-10 02:46:06 +0000

    [Update] userのeditページとupdate機能を作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'cd50209f57615ea557aa984ba96c94e2397939d00f88baf3c226a2801d66bc53f038eba2b0b91c894b1265ec17bb4598e0f310b5e7e2b4fe29faaf2db17ee31c'
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/homes/about' => 'homes#about'
   resources :books
-  resources :users, only: [:show,:index, :edit]
+  resources :users, only: [:show,:index, :edit, :update]
 end
