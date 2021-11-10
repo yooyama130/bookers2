6aa3ee787a4bc1b05f912b9538854b9f00670ee4
EC2 Default User <yooyama.97.01@gmail.com>
2021-11-10 05:13:09 +0000

    [Update] ユーザーの新規登録・プロフィール更新、本の新規投稿・編集の成功時のフラッシュメッセージ実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,17 @@ class ApplicationController < ActionController::Base
 
   # ログイン後のリダイレクト処理
   def after_sign_in_path_for(resource)
-    flash[:success] = 'Signed in successfully.'
     user_path(resource)
   end
 
   # ログアウト後のリダイレクト処理
   def after_sign_out_path_for(resource)
-    flash[:success] = 'Signed out successfully.'
     root_path
+  end
+
+  # アカウント登録後のリダイレクト処理
+  def after_sign_up_path_for(resource)
+    user_path(resource)
   end
 
   protected
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,6 +9,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
+    flash[:notice] = 'You have created book successfully.'
     redirect_to book_path(@book)
   end
 
@@ -25,9 +26,10 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = 'You have updated book successfully.'
     redirect_to book_path(book)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
