92f7ce3b33ee46e40b3d3e67b1c931f6603a758e
EC2 Default User <yooyama.97.01@gmail.com>
2021-11-11 08:19:47 +0000

    [Fix] データベースをリセットし再migrate

--- db/migrate/20211110083449_rename_body_column_to_users.rb
+++
@@ -1,5 +0,0 @@
-class RenameBodyColumnToUsers < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :users, :body, :introduction
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_10_083449) do
+ActiveRecord::Schema.define(version: 2021_11_09_004912) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -26,11 +26,11 @@ ActiveRecord::Schema.define(version: 2021_11_10_083449) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "name"
+    t.text "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.string "profile_image_id"
-    t.text "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
