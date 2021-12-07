e913d047a3ec9226646a4d1d9da9035bdc3e149b
EC2 Default User <yooyama.97.01@gmail.com>
2021-12-07 02:45:18 +0000

    [Update] 開発スキルアップ　ActiveStorage導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-  attachment :profile_image
+  has_one_attached :profile_image
 
   validates :name, uniqueness: true,length: {minimum: 2, maximum: 20}
   validates :introduction,length: {maximum: 50}
+  # validate :image_type
+
+private
+
+# def image_type
+#   if !image.blob.content_type.in?(%('image/jpeg image/png'))
+#     image.purge # Rails6では、この1行は必要ありません
+#     errors.add(:profile_image, 'はJPEGまたはPNG形式を選択してアップロードしてください')
+#   end
+# end
 end
